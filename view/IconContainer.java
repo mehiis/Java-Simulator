fb11565c825fadb9b3d683ea1942b76f3beb4279
aruraruri <aurilait@gmail.com>
2025-03-02 11:56:00 +0200

    commenting moar

--- view/IconContainer.java
+++ view/IconContainer.java
@@ -5,6 +5,10 @@ import javafx.scene.image.Image;
 import java.util.LinkedHashMap;
 import java.util.List;
 
+/*
+This class is currently not in use, idea was to have an ordered list for images to hang out in but idk.
+ */
+
 public class IconContainer {
     private LinkedHashMap<String, List<Image>> groupedList;
 
