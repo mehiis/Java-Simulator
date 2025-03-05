496a9dd295564531971573045a6ce9d43de4de23
Samuelms <samuel.sarimo@gmail.com>
2025-03-05 19:37:06 +0200

    Database script 1.0

---
+++ src/main/resources/DbScript.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE TRASH_SIMULATOR;
+USE TRASH_SIMULATOR;
+
+CREATE TABLE inputs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    date DATE NOT NULL,
+    simulation_time INT NOT NULL,
+    mean_trash_per_throw DOUBLE NOT NULL,
+    truck_arrival_interval INT NOT NULL,
+    single_apt_amount INT NOT NULL,
+    double_apt_amount INT NOT NULL,
+    triple_apt_amount INT NOT NULL,
+    quad_apt_amount INT NOT NULL,
+    mixed_amount INT NOT NULL,
+    plastic_amount INT NOT NULL,
+    bio_amount INT NOT NULL,
+    glass_amount INT NOT NULL,
+    paper_amount INT NOT NULL,
+    metal_amount INT NOT NULL
+);
+
+CREATE USER 'simulatoruser'@'localhost' IDENTIFIED BY 'password'; -- Change user and password
+GRANT SELECT, INSERT, UPDATE, DELETE ON inputs.* TO 'simulatoruser'@'localhost';
