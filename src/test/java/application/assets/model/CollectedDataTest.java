88adc57e62fb22a6197c40dc2e664c52fe1a07e2
beeboy98 <niko.mehilainen@gmail.com>
2025-03-07 18:40:06 +0200

    Added directory for future unit-testing classes.

---
+++ src/test/java/application/assets/model/CollectedDataTest.java
@@ -0,0 +1,55 @@
+package application.assets.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CollectedDataTest {
+
+    @Test
+    void addThrownThrash() {
+    }
+
+    @Test
+    void addGarbageCarArrival() {
+    }
+
+    @Test
+    void getGarbageCanCapacityPercentagesByType() {
+    }
+
+    @Test
+    void calculateThrashAmountByType() {
+    }
+
+    @Test
+    void calculateUsageRate() {
+    }
+
+    @Test
+    void getAverageUsageRateTotal() {
+    }
+
+    @Test
+    void getAverageRateOfType() {
+    }
+
+    @Test
+    void getHowManyTimeThrashThrown() {
+    }
+
+    @Test
+    void getGarbageCarArriveTimes() {
+    }
+
+    @Test
+    void getThrashTotalInLitres() {
+    }
+
+    @Test
+    void getThrashTotalInKg() {
+    }
+
+    @Test
+    void getThrashAmountByType() {
+    }
+}
